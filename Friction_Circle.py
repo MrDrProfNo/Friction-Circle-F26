82b76b8faf0afa47fa8483c6fdc18a49eb7ebfe5
Arthur Heiles <amh8850@g.rit.edu>
2018-03-24 10:50:15 -0400

    - moved readParseToExcel and replaceMitchellVariables to forces_parse.py and added documentation for the latter. The file now contains all function used in producing the final excel file.  - adjusted some import statements to be more accurate  - changed df_to_excel to remove static class; it added nothing.

--- Friction_Circle.py
+++ Friction_Circle.py
@@ -29,18 +29,29 @@ from decimal import Decimal
 from numpy import *
 from pandas import *
 
-import forces_parser
-
+from forces_parser import parse, readParseToExcel
 from force_calculator import createDataframe
-
 from friction_circle_init import init
-
+from df_to_excel import full_dump, formatted_dump
+from macro import macroMain
 
 ## Constants
 CONFIG_FILE = "carConfig.txt"
 
 
 def getDimensionForceDataframes(fullDF):
+	"""
+	Separates out 3 dataframes from the full one; these dataframes are 
+	specifically used later in the program, and as such the columns separated
+	are hardcoded.
+	The three sets are:
+	LF_Fx, RF_Fx, LR_Fx, RR_Fx;
+	LF_Fy, RF_Fy, LR_Fy, RR_Fy;
+	LF_Fz, RF_Fz, LR_Fz, RR_Fz;
+
+	:param fullDF: the complete Dataframe from force_calculator
+	:return: 3-tuple of the separated dataframes x,y,z
+	"""
 	# Create dataframe for x forces
 	df_MCPFx = DataFrame(fullDF.loc[:, ["LF_Fx", "RF_Fx", "LR_Fx", "RR_Fx"] ])
 	# Create dataframe for y forces
@@ -49,210 +60,7 @@ def getDimensionForceDataframes(fullDF):
 	df_MCPFz = DataFrame(fullDF.loc[:, ["LF_Fz", "RF_Fz", "LR_Fz", "RR_Fz"] ])
 	return df_MCPFx, df_MCPFy, df_MCPFz
 
-def readParseToExcel(fromFileFront, fromFileRear, toFile, df_full):
-	"""
-	Takes
-	:return:
-	"""
-	try:
-		f = open(fromFileFront)
-	except:
-		print("Error in opening file:", fromFileFront,
-			  "\n Could not find file.")
-	line = f.readline()
-	# Skip leading newlines in input file
-	while (line == "\n"):
-		line = f.readline()
 
-	leftMargin = 3  # left margin of tables
-	topMargin = 3  # top margin for tables (must be at least 1)
-	displacement = 0  # number of tables from left; 0 indexed
-	xspacing = 3  # horizontal space between each table
-	yspacing = 3  # vertical space between each table
-	tablewidth = 5  # width of each table
-	tableheight = 13  # height of each table
-	count = 0  # number of tables read so far
-
-	writer = ExcelWriter(toFile, engine='xlsxwriter')
-	dataDict = {}
-
-	while (True):
-		if (line == "\n"):
-			if (displacement == 1 or displacement == 0):
-				columnArray = [
-					"Values",
-					"Lower Outboard @ Upright",
-					"Upper Outboard @ Upright",
-					"Steering @ Upright",
-					"Front Lower Front @ Chassis",
-					"Front Lower Rear @ Chassis",
-					"Push Rod @ Bellcrank",
-					"Front Upper Front @ Chassis",
-					"Front Upper Rear @ Chassis",
-					"Bell Crank Pivot @ Post",
-					"Bell Crank Pivot @ Post",
-					"Shock @ Chassis"
-				]
-			else:
-				columnArray = [
-					"Values",
-					"Lower Outboard @ Upright",
-					"Upper Outboard @ Upright",
-					"Toe Link @ Upright",
-					"Rear Lower Front @ Chassis",
-					"Rear Lower Rear @ Chassis",
-					"Push Rod @ Bellcrank",
-					"Rear Upper Front @ Chassis",
-					"Rear Upper Rear @ Chassis",
-					"Forward Bell Crank Pivot @ Post",
-					"Bell Crank Pivot @ Post",
-					"Shock @ Chassis"
-				]
-
-			df = DataFrame(dataDict, columns=columnArray,
-						   index=["X", "Y", "Z", "Totals"])
-			df = df.transpose()
-
-			# Title text: The text written above each table
-			titleText = ""
-			# Alternate column displacement between left and right wheels
-			# 0 and 1 cases are for front wheels; 2 and 3 are for rear
-			caseIDX = (count // 2)
-			caseData = df_full.iloc[caseIDX]
-
-			# case = Decimal(case)
-			# case = round(case, 1)
-			if (displacement == 0):  # right front wheel
-				displacement = 1
-				titleText += "Right Front "
-				df.loc["Values", "X":"Z"] = caseData["RF_Fx"], \
-											caseData["RF_Fy"], \
-											caseData["RF_Fz"],
-			elif (displacement == 1):  # left front wheel
-				displacement = 0
-				titleText += "Left Front "
-				df.loc["Values", "X":"Z"] = caseData["LF_Fx"], \
-											caseData["LF_Fy"], \
-											caseData["LF_Fz"]
-			elif (displacement == 2):  # right rear wheel
-				displacement = 3
-				titleText += "Right Rear "
-				df.loc["Values", "X":"Z"] = caseData["RR_Fx"], \
-											caseData["RR_Fy"], \
-											caseData["RR_Fz"]
-			elif (displacement == 3):  # left rear wheel
-				displacement = 2
-				titleText += "Left Rear "
-				df.loc["Values", "X":"Z"] = caseData["LR_Fx"], \
-											caseData["LR_Fy"], \
-											caseData["LR_Fz"]
-
-			df.to_excel(writer, sheet_name="Sheet 1",
-						startcol=leftMargin + (
-						displacement * (tablewidth + xspacing)),
-						startrow=topMargin + (
-						(count // 2) * (yspacing + tableheight))
-						)
-
-			case = (count // 2)
-
-			# occasionally spits a message into console, but never crashes
-			# message roughly "non-integers will soon cease to be supported"
-			case_theta_coef = df_full.index[case]
-
-			titleText += case_theta_coef.to_eng_string()
-			ws = writer.sheets["Sheet 1"]
-			ws.write(
-				topMargin + ((count // 2) * (yspacing + tableheight)) - 1,
-				leftMargin + (displacement * (tablewidth + xspacing)) + 1,
-				titleText
-			)
-
-			count += 1
-			line = f.readline()
-			dataDict = {}
-			continue
-
-		if (line == ""):
-			if (f.name == fromFileFront):  # End of front wheel file
-				f.close()
-				try:
-					f = open(fromFileRear)
-				except:
-					print("Error in opening file:", fromFileRear, "\n "
-																  "File may not exist")
-
-				count = 0
-				displacement = 3
-				line = f.readline()
-				# Skip leading newlines
-				while (line == "\n"):
-					line = f.readline()
-			else:  # already on rear file
-				f.close()
-				break
-
-		line = line.split(",")
-		if (line[0] == "N" or line[0] == "V" or
-					replaceMitchellVariables(line[0]) in dataDict):
-			line = f.readline()
-			continue
-
-		if (displacement <= 1):
-			line[0] = replaceMitchellVariables(line[0], False)
-		else:
-			line[0] = replaceMitchellVariables(line[0], True)
-
-		line[1] = Decimal(line[1])
-		line[2] = Decimal(line[2])
-		line[3] = Decimal(line[3])
-		dataDict[line[0]] = [line[1], line[2], line[3],
-							 "=SQRT(INDIRECT(\"C[-1]\","
-							 " FALSE)^2 +INDIRECT(\"C[-2]\", FALSE)^2 + "
-							 "INDIRECT(\"C[-3]\", FALSE)^2)"
-							 ]
-		line = f.readline()
-
-	writer.close()
-
-
-def replaceMitchellVariables(var, rear=False):
-	if (var == "B"):
-		return "Lower Outboard @ Upright"
-	elif (var == "E"):
-		return "Upper Outboard @ Upright"
-	elif (var == "S"):
-		if (rear):
-			return "Toe Link @ Upright"
-		return "Steering @ Upright"
-	elif (var == "A"):
-		if (rear):
-			return "Rear Lower Front @ Chassis"
-		return "Front Lower Front @ Chassis"
-	elif (var == "C"):
-		if (rear):
-			return "Rear Lower Rear @ Chassis"
-		return "Front Lower Rear @ Chassis"
-	elif (var == "MW"):
-		return "Push Rod @ Bellcrank"
-	elif (var == "D"):
-		if (rear):
-			return "Rear Upper Front @ Chassis"
-		return "Front Upper Front @ Chassis"
-	elif (var == "F"):
-		if (rear):
-			return "Rear Upper Rear @ Chassis"
-		return "Front Upper Rear @ Chassis"
-	elif (var == "P"):
-		if (rear):
-			return "Forward Bell Crank Pivot @ Post"
-		return "Forward Bell Crank Pivot @ Post"
-	elif (var == "Q"):
-		return "Bell Crank Pivot @ Post"
-	elif (var == "W"):
-		return "Shock @ Chassis"
-	else:
-		print("Unrecognized Mitchell Variable: ", var)
 
 
 def main():
@@ -268,20 +76,20 @@ def main():
 
 
 	outputFolderPath = "./Friction_Circle_F26_Output/"
-	DFToExcel.full_dump(outputFolderPath, "dump.xlsx", fullDF)
-	DFToExcel.formatted_dump(outputFolderPath, "formatting.xlsx", dim_force_dfs)
+	full_dump(outputFolderPath, "dump.xlsx", fullDF)
+	formatted_dump(outputFolderPath, "formatting.xlsx", dim_force_dfs)
 
-	# input("Ensure that vDosPlus running Mitchell is open before continuing.\n"
-	# 	  "Press Enter to continue:")
+	input("Ensure that vDosPlus running Mitchell is open before continuing.\n"
+		  "Press Enter to continue:")
 
-	# # Run the macro
-	# macroMain(dim_force_dfs)
+	# Run the macro
+	macroMain(dim_force_dfs)
 
-	forces_parser.main("tmpF.txt", "FOutput.txt")
-	forces_parser.main("tmpR.txt", "ROutput.txt")
+	parse("tmpF.txt", "FOutput.txt")
+	parse("tmpR.txt", "ROutput.txt")
 
-	# readParseToExcel("FOutput.txt", "ROutput.txt", "./Parsed_Forces.xlsx",
-	# 				 fullDF)
+	readParseToExcel("FOutput.txt", "ROutput.txt", "./Parsed_Forces.xlsx",
+	 				 fullDF)
 
 
 if __name__ == "__main__":
--- forces_parser.py
+++ forces_parser.py
@@ -3,9 +3,10 @@
 import argparse
 import os.path
 import re
+from decimal import Decimal
+from pandas import ExcelWriter, DataFrame
 
-
-def main(path, output_name):
+def parse(path, output_name):
 	print('Opening input text file...')
 	f = open(path).readlines()
 	print('Text file open!')
@@ -49,7 +50,242 @@ def main(path, output_name):
 	print('All done!')
 
 
+
+def readParseToExcel(fromFileFront, fromFileRear, toFile, df_full):
+	"""
+	Takes
+	:return:
+	"""
+	try:
+		f = open(fromFileFront)
+	except:
+		print("Error in opening file:", fromFileFront,
+			  "\n Could not find file.")
+	line = f.readline()
+	# Skip leading newlines in input file
+	while (line == "\n"):
+		line = f.readline()
+
+	leftMargin = 3  # left margin of tables
+	topMargin = 3  # top margin for tables (must be at least 1)
+	displacement = 0  # number of tables from left; 0 indexed
+	xspacing = 3  # horizontal space between each table
+	yspacing = 3  # vertical space between each table
+	tablewidth = 5  # width of each table
+	tableheight = 13  # height of each table
+	count = 0  # number of tables read so far
+
+	writer = ExcelWriter(toFile, engine='xlsxwriter')
+	dataDict = {}
+
+	while (True):
+		if (line == "\n"):
+			if (displacement == 1 or displacement == 0):
+				columnArray = [
+					"Values",
+					"Lower Outboard @ Upright",
+					"Upper Outboard @ Upright",
+					"Steering @ Upright",
+					"Front Lower Front @ Chassis",
+					"Front Lower Rear @ Chassis",
+					"Push Rod @ Bellcrank",
+					"Front Upper Front @ Chassis",
+					"Front Upper Rear @ Chassis",
+					"Bell Crank Pivot @ Post",
+					"Bell Crank Pivot @ Post",
+					"Shock @ Chassis"
+				]
+			else:
+				columnArray = [
+					"Values",
+					"Lower Outboard @ Upright",
+					"Upper Outboard @ Upright",
+					"Toe Link @ Upright",
+					"Rear Lower Front @ Chassis",
+					"Rear Lower Rear @ Chassis",
+					"Push Rod @ Bellcrank",
+					"Rear Upper Front @ Chassis",
+					"Rear Upper Rear @ Chassis",
+					"Forward Bell Crank Pivot @ Post",
+					"Bell Crank Pivot @ Post",
+					"Shock @ Chassis"
+				]
+
+			df = DataFrame(dataDict, columns=columnArray,
+						   index=["X", "Y", "Z", "Totals"])
+			df = df.transpose()
+
+			# Title text: The text written above each table
+			titleText = ""
+			# Alternate column displacement between left and right wheels
+			# 0 and 1 cases are for front wheels; 2 and 3 are for rear
+			caseIDX = (count // 2)
+			caseData = df_full.iloc[caseIDX]
+
+			# case = Decimal(case)
+			# case = round(case, 1)
+			if (displacement == 0):  # right front wheel
+				displacement = 1
+				titleText += "Right Front "
+				df.loc["Values", "X":"Z"] = caseData["RF_Fx"], \
+											caseData["RF_Fy"], \
+											caseData["RF_Fz"],
+			elif (displacement == 1):  # left front wheel
+				displacement = 0
+				titleText += "Left Front "
+				df.loc["Values", "X":"Z"] = caseData["LF_Fx"], \
+											caseData["LF_Fy"], \
+											caseData["LF_Fz"]
+			elif (displacement == 2):  # right rear wheel
+				displacement = 3
+				titleText += "Right Rear "
+				df.loc["Values", "X":"Z"] = caseData["RR_Fx"], \
+											caseData["RR_Fy"], \
+											caseData["RR_Fz"]
+			elif (displacement == 3):  # left rear wheel
+				displacement = 2
+				titleText += "Left Rear "
+				df.loc["Values", "X":"Z"] = caseData["LR_Fx"], \
+											caseData["LR_Fy"], \
+											caseData["LR_Fz"]
+
+			df.to_excel(writer, sheet_name="Sheet 1",
+						startcol=leftMargin + (
+						displacement * (tablewidth + xspacing)),
+						startrow=topMargin + (
+						(count // 2) * (yspacing + tableheight))
+						)
+
+			case = (count // 2)
+
+			# occasionally spits a message into console, but never crashes
+			# message roughly "non-integers will soon cease to be supported"
+			case_theta_coef = df_full.index[case]
+
+			titleText += case_theta_coef.to_eng_string()
+			ws = writer.sheets["Sheet 1"]
+			ws.write(
+				topMargin + ((count // 2) * (yspacing + tableheight)) - 1,
+				leftMargin + (displacement * (tablewidth + xspacing)) + 1,
+				titleText
+			)
+
+			count += 1
+			line = f.readline()
+			dataDict = {}
+			continue
+
+		if (line == ""):
+			if (f.name == fromFileFront):  # End of front wheel file
+				f.close()
+				try:
+					f = open(fromFileRear)
+				except:
+					print("Error in opening file:", fromFileRear, "\n "
+																  "File may not exist")
+
+				count = 0
+				displacement = 3
+				line = f.readline()
+				# Skip leading newlines
+				while (line == "\n"):
+					line = f.readline()
+			else:  # already on rear file
+				f.close()
+				break
+
+		line = line.split(",")
+		if (line[0] == "N" or line[0] == "V" or
+					replaceMitchellVariables(line[0]) in dataDict):
+			line = f.readline()
+			continue
+
+		if (displacement <= 1):
+			line[0] = replaceMitchellVariables(line[0], False)
+		else:
+			line[0] = replaceMitchellVariables(line[0], True)
+
+		line[1] = Decimal(line[1])
+		line[2] = Decimal(line[2])
+		line[3] = Decimal(line[3])
+		dataDict[line[0]] = [line[1], line[2], line[3],
+							 "=SQRT(INDIRECT(\"C[-1]\","
+							 " FALSE)^2 +INDIRECT(\"C[-2]\", FALSE)^2 + "
+							 "INDIRECT(\"C[-3]\", FALSE)^2)"
+							 ]
+		line = f.readline()
+
+	writer.close()
+
+
+def replaceMitchellVariables(var, rear=False):
+	"""
+	Helper function for readParseToExcel()
+	Takes in a variable from Mitchell and replaces it with the associated word.
+	These associations were all grabbed from the original excel file with the
+	final calculated forces in it (which readParseToExcel is producing a copy
+	of).
+	Some associations vary depending on whether the data is for the front or the
+	rear axle; if the passed boolean is True, the rear-axle associations are
+	used.
+	A list of valid mitchell variables follows:
+	B
+	E
+	S
+	A
+	C
+	MW
+	D
+	F
+	P
+	Q
+	W
+
+
+	:param var: string from the above list of valid mitchell variables
+	:param rear: boolean; true to use rear-axle phrase associations
+	:return: The string to replace the passed variable with
+	"""
+	if (var == "B"):
+		return "Lower Outboard @ Upright"
+	elif (var == "E"):
+		return "Upper Outboard @ Upright"
+	elif (var == "S"):
+		if (rear):
+			return "Toe Link @ Upright"
+		return "Steering @ Upright"
+	elif (var == "A"):
+		if (rear):
+			return "Rear Lower Front @ Chassis"
+		return "Front Lower Front @ Chassis"
+	elif (var == "C"):
+		if (rear):
+			return "Rear Lower Rear @ Chassis"
+		return "Front Lower Rear @ Chassis"
+	elif (var == "MW"):
+		return "Push Rod @ Bellcrank"
+	elif (var == "D"):
+		if (rear):
+			return "Rear Upper Front @ Chassis"
+		return "Front Upper Front @ Chassis"
+	elif (var == "F"):
+		if (rear):
+			return "Rear Upper Rear @ Chassis"
+		return "Front Upper Rear @ Chassis"
+	elif (var == "P"):
+		if (rear):
+			return "Forward Bell Crank Pivot @ Post"
+		return "Forward Bell Crank Pivot @ Post"
+	elif (var == "Q"):
+		return "Bell Crank Pivot @ Post"
+	elif (var == "W"):
+		return "Shock @ Chassis"
+	else:
+		print("Unrecognized Mitchell Variable: ", var)
+
 if __name__ == '__main__':
+	# if this is being run as main, asks for a file to read from and produces
+	# the parsed files
 	parser = argparse.ArgumentParser()
 	parser.add_argument('path', type=str,
 						help='Path to the input file. Must be the complete file path!')
@@ -57,4 +293,4 @@ if __name__ == '__main__':
 						help='Name for the output file.')
 	args = parser.parse_args()
 
-	main(args.path, args.output_name)
+	parse(args.path, args.output_name)
