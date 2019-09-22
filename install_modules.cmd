4b6ec4f790bae28a65a1847d85b06cf74ee771e2
aheiles <32115458+MrDrProfNo@users.noreply.github.com>
2019-09-22 15:19:13 -0400

    Forced correct version of pandas for python 3.5.2

--- install_modules.cmd
+++ install_modules.cmd
@@ -1,10 +1,11 @@
 title Install Modules
 
 python -m pip install keyboard==0.11.0
-python -m pip install pandas
+python -m pip install pandas==0.24.2
 python -m pip install xlsxwriter
 python -m pip install win32gui
 python -m pip install pywin32
+python -m pip install xlrd
 
 pause
 exit
