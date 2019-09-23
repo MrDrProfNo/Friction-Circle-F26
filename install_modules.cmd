edda864b1fec22d1139d0f21b6b9d96760771abd
aheiles <32115458+MrDrProfNo@users.noreply.github.com>
2019-09-23 00:24:17 -0400

    Updated setup cmd to force versions compatible w/ python 3.5.2

--- install_modules.cmd
+++ install_modules.cmd
@@ -2,10 +2,10 @@ title Install Modules
 
 python -m pip install keyboard==0.11.0
 python -m pip install pandas==0.24.2
-python -m pip install xlsxwriter
-python -m pip install win32gui
-python -m pip install pywin32
-python -m pip install xlrd
+python -m pip install xlsxwriter==1.2.1
+python -m pip install win32gui==221.6
+python -m pip install pywin32==225
+python -m pip install xlrd==1.2.0
 
 pause
 exit
