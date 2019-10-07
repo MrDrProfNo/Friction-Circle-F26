c49f0761e5a4d5d4e098292b9a1967d11d45f888
aheiles <32115458+MrDrProfNo@users.noreply.github.com>
2019-10-06 23:57:51 -0400

    Moved to python 3.7, updated readme to match.

--- install_modules.cmd
+++ install_modules.cmd
@@ -1,11 +1,11 @@
 title Install Modules
 
-python -m pip install keyboard==0.11.0
-python -m pip install pandas==0.24.2
-python -m pip install xlsxwriter==1.2.1
-python -m pip install win32gui==221.6
-python -m pip install pywin32==225
-python -m pip install xlrd==1.2.0
+py -m pip install keyboard==0.11.0
+py -m pip install pandas==0.25.1
+py -m pip install xlsxwriter==1.2.1
+::py -m pip install win32gui==221.6 ::no longer required
+py -m pip install pywin32==225
+py -m pip install xlrd==1.2.0
 
 pause
 exit
